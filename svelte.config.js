aef6cc047c944b95c9abd08e47265d1bb04459b7
Daniel Melton <danmelt30@gmail.com>
2022-04-04 22:21:53 -0500

    Changed to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,28 @@
-import node from '@sveltejs/adapter-node';
+// import node from '@sveltejs/adapter-node';
+// import preprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+
+// 	kit: {
+// 		adapter: node()
+// 	}
+// };
+
+// export default config;
+
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: node()
+		adapter: vercel()
 	}
 };
 
