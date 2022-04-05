b3cccc4828505df134f1658bbb1a2a59a2080f19
Daniel Melton <danmelt30@gmail.com>
2022-04-04 22:38:08 -0500

    file added

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,17 @@
-// import node from '@sveltejs/adapter-node';
-// import preprocess from 'svelte-preprocess';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://github.com/sveltejs/svelte-preprocess
-// 	// for more information about preprocessors
-// 	preprocess: preprocess(),
-
-// 	kit: {
-// 		adapter: node()
-// 	}
-// };
-
-// export default config;
-
-import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+
 	kit: {
-		adapter: vercel()
+		adapter: node({'out': 'build'})
 	}
 };
 
 export default config;
+
+
