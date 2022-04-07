5259e7b1359395384209b58b4cf97a9990732521
Daniel Melton <danmelt30@gmail.com>
2022-04-06 19:58:29 -0500

    Image resized

--- src/components/headerMartian.svelte
+++ src/components/headerMartian.svelte
@@ -1,27 +1,22 @@
-<script>
-	import backgroundImg from '../assets/images/backgroundImage.jpg';
-</script>
-
-<section
-	class="h-screen -mt-1 flex place-content-center bg-local bg-bottom bg-cover bg-no-repeat"
-	style="background-image: url({backgroundImg});"
->
-	<h1
-		class="text-6xl sm:text-8xl mt-60 text-white font-black text-center drop-shadow-xl "
-	>
+<section class="h-screen -mt-1 flex place-content-center bg-cover bg-no-repeat sm:bg-center">
+	<h1 class="text-6xl sm:text-8xl mt-48 sm:mt-60 text-white font-black text-center drop-shadow-xl ">
 		Miami Martian Club
 	</h1>
-	<!-- <button>Buy Now</button> -->
 </section>
 
-
 <style>
-	h1{
+	section {
+		background-image: url('../assets/images/backgroundImgSmall.jpg');
+	}
+	h1 {
 		text-shadow: 0.25rem 0.5rem #40f6a4;
 	}
 	@media only screen and (max-width: 600px) {
-		h1{
-		text-shadow: 0.05rem 0.35rem #40f6a4;
-	}
+		section {
+			background-image: url('../assets/images/bgImg2.jpg');
+		}
+		h1 {
+			text-shadow: 0.05rem 0.35rem #40f6a4;
+		}
 	}
 </style>
